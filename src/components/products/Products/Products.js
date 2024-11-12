703c7e31b081cf8b4866e47e81ff805d3122d347
nikashabashvili <shabashvilinika@yahoo.com>
2024-11-12 18:56:18 +0400

    profile

--- src/components/products/Products/Products.js
+++ src/components/products/Products/Products.js
@@ -27,10 +27,6 @@ const Products = () => {
     }))
   }, [dispatch, categoryId])
 
-
-  console.log(subCategories)
-  if(products.status === 'loading') return <div>...</div>
-
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -82,38 +78,50 @@ const Products = () => {
         
 
         <div className={styles.list}>
-            {products?.data?.length > 0 ? products.data.map(product => (
-              <div className={styles.item} key={product._id}>
-                <div className={styles.imager}>
-                  <Link to={product._id}>
-                    <img src={product.images[0] || 'https://img.freepik.com/free-vector/white-product-podium-with-green-tropical-palm-leaves-golden-round-arch-green-wall_87521-3023.jpg'} alt={product.name} />
-                    <div className={styles.additional}>
-                      <RiShoppingCart2Line className={styles.cartIcon} />
-                    </div>
-                    <div className={styles.serialnumber}>{product.prod_id}</div>
-                    <div className={styles.name}>
-                      {product.name} <br/> {product.description}
-                      {/* {!product.isNew && <span className={styles.newLabel}> ახალი </span>} */}
-                    </div>
-                  </Link>
-                  <div className={styles.price}>{product.price}ლ</div>
-                  <div className={styles.date}>{new Date(product.createdAt).toISOString().split("T")[0]}</div>
-                  <div className={`${styles.stockStatus} ${product.quantity > 0 ? styles.inStock : styles.outOfStock}`}>
-                    {product.quantity > 0 ? (
-                      <>
-                        <FaCheck className={styles.checkIcon} />
-                        მარაგშია {product.quantity}
-                      </>
-                    ) : (
-                      <>
-                        <ImCross className={styles.crossIcon} />
-                        ამოიწურა
-                      </>
-                    )}
-                  </div>
+            {products.status === 'loading' 
+              ? [1,2,3,4,5].map((item) => (
+                <div className={styles.item} style={{background: 'gray'}}>
+                  
                 </div>
-              </div>
-            )) : <div>Products Are Empty!</div>}
+              ))
+              : products?.data?.length > 0 ? (
+                products.data.map(product => (
+                  <div className={styles.item} key={product._id}>
+                    <div className={styles.imager}>
+                      <Link to={product._id}>
+                        <img src={product.images[0] || 'https://img.freepik.com/free-vector/white-product-podium-with-green-tropical-palm-leaves-golden-round-arch-green-wall_87521-3023.jpg'} alt={product.name} />
+                        <div className={styles.additional}>
+                          <RiShoppingCart2Line className={styles.cartIcon} />
+                        </div>
+                        <div className={styles.serialnumber}>{product.prod_id}</div>
+                        <div className={styles.name}>
+                          {product.name} <br/> {product.description}
+                          {/* {!product.isNew && <span className={styles.newLabel}> ახალი </span>} */}
+                        </div>
+                      </Link>
+                      <div className={styles.price}>{product.price}ლ</div>
+                      <div className={styles.date}>{new Date(product.createdAt).toISOString().split("T")[0]}</div>
+                      <div className={`${styles.stockStatus} ${product.quantity > 0 ? styles.inStock : styles.outOfStock}`}>
+                        {product.quantity > 0 ? (
+                          <>
+                            <FaCheck className={styles.checkIcon} />
+                            მარაგშია {product.quantity}
+                          </>
+                        ) : (
+                          <>
+                            <ImCross className={styles.crossIcon} />
+                            ამოიწურა
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                  <div>Empty</div>
+              )
+            
+            }
           </div>
 
           <div className={styles.pagination}>
